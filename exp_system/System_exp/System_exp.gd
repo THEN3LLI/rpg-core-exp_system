f8f790dbe4a6235d05bca695f9aae37e0401571f
THEN3LLI <danielecinellidev@gmail.com>
2022-12-01 20:14:17 +0100

    -

--- exp_system/System_exp/System_exp.gd
+++ exp_system/System_exp/System_exp.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var class_modifier_values = [1,1,1,1,5] #health, stamina, magika, exp modifier, ability unlock every lvl
+var class_modifier_values = [1,1,1,1] #health, stamina, magika, exp modifier, ability unlock every lvl
 var entity_exp
 var entity_exp_cap = 100
 var entity_level = 1
@@ -10,23 +10,34 @@ func _ready():
 	entity_exp = 0
 	entity_exp_cap = 100
 	entity_level = 1
-	
-func _level_up():
-	_exp_cap()
+
 ## get exp and sum to entity exp
 func _get_exp(var i):
-	
 	entity_exp +=i
 	if entity_exp_cap<= entity_exp:
+		var exp_overflow = i-entity_exp_cap
 		_level_up()
 		
-func _attribute_increase():pass
-func _ability_unlock():pass
+		entity_exp = 0
+		_get_exp(exp_overflow)
+func _level_up():
+	
+	entity_level +=1
+	
+	#_attribute_increase()
+	_exp_cap()
+func _exp_cap():
+	entity_exp_cap = 100*entity_level*1.5*entity_level/2*class_modifier_values[3]
+	
+	
 func _level_cap():pass
 ## EXP CAP based on level and class modifier
-func _exp_cap():
-	entity_exp_cap = entity_level*1.5*entity_level/2*class_modifier_values[4]
+		
+func _attribute_increase():
+	get_parent()._set_attribute(class_modifier_values)
 	
+#func _ability_unlock():pass
+
 ## Class modifier inherith by parent class that modify exp gains, hp, stamina etc
 func _class_modifiers(var i):
 	class_modifier_values = i
